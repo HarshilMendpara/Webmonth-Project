5ac011f3a1ded66cf16ac195725ecae965a16237
Harshil <mendparaharshil@gmail.com>
2021-05-12 09:34:57 +0530

    signup and signin screen added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const body = document.querySelector("body");
+const signin = document.querySelector(".sign-in");
+const signup = document.querySelector(".sign-up");
 
 window.addEventListener("load", () => {
   body.classList.add("visible");
